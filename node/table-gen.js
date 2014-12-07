daf44814abf382cde52e3b322ce62fbd3f99accb
Ivan Hall <ivan.hall@gmail.com>
2014-12-07 08:28:31 -0500

    Transposed.

--- node/table-gen.js
+++ node/table-gen.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 
 var Handlebars = require('handlebars');
-console.log(Handlebars);
+//console.log(Handlebars);
 
 var gcisData = [];
 var gcisLookup = {};
@@ -25,9 +25,7 @@ function readTumorsFile() {
     fileLines[0].split(',').forEach(function(element, index) {
         if(index != 0) {
             gcisData.push({
-                "tumors": [],
-                "id": element.trim(),
-                "tumorCount": 0
+                "id": element.trim()
             });
 
             gcisLookup[element] = gcisData.length - 1;
@@ -43,7 +41,11 @@ function readTumorsFile() {
 
         if(tumorName.length > 0) {
 
-            tumorData.push({ "name": tumorName });
+            tumorData.push({
+                "name": tumorName,
+                "tumors": [],
+                "tumorCount": 0
+            });
 
             lineColumns.shift();
 
@@ -65,6 +67,59 @@ function readTumorsFile() {
     console.log(tumorData);
 }
 
+var geneColumns = [];
+var tumorRows = [];
+
+function readTumorsFileTransposed() {
+    var fileName = __dirname + '\\' + 'tumors.csv';
+    var fileContents = fs.readFileSync(fileName).toString();
+    var fileLines = fileContents.split(/\n/);
+
+    fileLines[0].split(',').forEach(function(element, index) {
+        if(index != 0) {
+            geneColumns.push({
+                "id": element.trim(),
+                "tumorCount": 0
+            });
+        }
+    });
+
+    fileLines.shift();
+
+    fileLines.forEach(function(element, index) {
+        lineColumns = element.split(',');
+
+        tumorName = lineColumns[0].trim();
+
+        if(tumorName.length > 0) {
+
+            tumorRows.push({
+                "name": tumorName,
+                "genes": []
+            });
+            tumorRowIndex = tumorRows.length - 1;
+
+            lineColumns.shift();
+
+            lineColumns.forEach(function(element, index) {
+                element = element.trim();
+                if(element === 'x') {
+                    tumorRows[tumorRowIndex]["genes"].push({ "hasTumor": true });
+                    geneColumns[index]["tumorCount"]++;
+                }
+                else {
+                    tumorRows[tumorRowIndex]["genes"].push({ "hasTumor": false });
+                }
+            });
+        }
+    });
+
+    console.log("*** Gene Count: " + geneColumns.length + "\n");
+    console.log("*** Tumor Count: " + tumorRows.length + "\n");
+    //debugGcisData();
+    //console.log(tumorRows);
+}
+
 function buildHtmlFile() {
 
     var fileContents = fs.readFileSync("template.html").toString();
@@ -81,5 +136,25 @@ function buildHtmlFile() {
     });
 }
 
-readTumorsFile();
-buildHtmlFile();
+function buildHtmlFileTransposed() {
+
+    var fileContents = fs.readFileSync("template-2.html").toString();
+    var template = Handlebars.compile(fileContents);
+
+    var dataHtml = template({
+        "tumorRows": tumorRows,
+        "geneColumns": geneColumns
+    })
+
+    fs.writeFile(__dirname + "\\..\\" + "data-2.html", dataHtml, function(error) {
+        if (error) throw error
+        console.log("data-2.html is written!");
+    });
+}
+
+
+//readTumorsFile();
+//buildHtmlFile();
+
+readTumorsFileTransposed();
+buildHtmlFileTransposed();
